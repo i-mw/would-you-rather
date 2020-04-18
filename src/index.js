068777b36a420ddb1658440980d80b4025dc5557
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2020-04-18 18:04:13 +0200

    feat: Add initial data reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./components/App"
-import { createStore } from "redux"
-import {Provider} from "react-redux"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 
-const store = createStore()
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
