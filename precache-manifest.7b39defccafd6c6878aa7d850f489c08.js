ec0bb439849d221eacccaa341c963a7579264711
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2020-04-20 17:56:46 +0200

    Updates

---
+++ precache-manifest.7b39defccafd6c6878aa7d850f489c08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0747d5ae4785a5a8efd80f01ecb7de5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e5212312404c648cde1",
+    "url": "./static/css/main.63381e24.chunk.css"
+  },
+  {
+    "revision": "92674325f58748c49565",
+    "url": "./static/js/2.6c37b970.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6c37b970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5212312404c648cde1",
+    "url": "./static/js/main.04f742da.chunk.js"
+  },
+  {
+    "revision": "926f2ff8a7ed89dfd94f",
+    "url": "./static/js/runtime-main.b5ca9d78.js"
+  }
+]);
